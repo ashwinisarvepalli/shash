9e2c698a5e30ebde54ae005dad352c12ab3b4a53
Ashwini Sarvepalli <ashwini1001@gmail.com>
2021-10-24 14:26:00 -0500

    creating map chart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
+import MapChart from './components/MapChart.js'
+import {
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h4> Where's My Water </h4>
+        <div className="pages">
+          <Link></Link>
+          <Link to="/Case Studies" className="item-but">Case Studies</Link>
+          <Link to="/AboutUs" className="item-but" >Sponsors/Partnerships</Link>
+          <Link to="/GalleryView" className="item-but" >Gallery View</Link>
+        </div>
       </header>
-    </div>
+        <Switch>
+          <Route exact path="/ListView">
+            <ListView />
+          </Route>
+          <Route exact path="/GalleryView">
+            <GalleryView />
+          </Route>
+          <Route exact path="/Details/:name">
+            <Details />
+          </Route>
+        </Switch> 
+      </div>
+      <MapChart/>
   );
 }
 
